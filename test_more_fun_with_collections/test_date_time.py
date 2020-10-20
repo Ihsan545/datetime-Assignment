163cf56a7ad3523aeaa60a84f54742373694c9a4
anwar <Ihsan545>
2020-10-19 19:14:49 -0500

    Test passed

--- test_more_fun_with_collections/test_date_time.py
+++ test_more_fun_with_collections/test_date_time.py
@@ -8,7 +8,7 @@ from more_fun_with_collections import date_time
 class MyTestCase(unittest.TestCase):
     def test_half_birthday(self):
         """ tests half_birthday."""
-        birthday = datetime.date()
+        birthday = datetime.date(2020, 1, 1)
         later_half_birthday = datetime.date(2020, 7, 1)
         self.assertEqual(date_time.half_birthday(birthday), later_half_birthday)
 
